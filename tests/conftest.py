6a3005cddfbb07cefc897dae181767f0322baba6
v.osipov <v.osipov@a1qa.com>
2022-04-11 15:22:43 +0300

    Add common assertion methods

--- tests/conftest.py
+++ tests/conftest.py
@@ -28,7 +28,7 @@ def clear_all_data_before_test_run(api_object):
 def create_bear_with_valid_data(api_object):
     name = f'test_name_{DatetimeUtils.timestamp()}'
     age = BEAR_AGE_MIN + BEAR_AGE_RANGE * random.random()
-    bear_type = random.choice(list(BearType.__dict__.values()))
+    bear_type = random.choice([getattr(BearType, attr) for attr in dir(BearType) if not attr.startswith('__')])
     bear_model = Bear(bear_type=bear_type, bear_name=name, bear_age=age, bear_id=None)
     response = api_object.create_bear(bear_model)
     if not response.text.isnumeric():
--- tests/test_alaska_bears.py
+++ tests/test_alaska_bears.py
@@ -11,6 +11,19 @@ import support.constants as constants
 
 
 class TestAlaskaBears:
+    @staticmethod
+    def assert_response_is_in_json_format(response):
+        assert_that(calling(response.json), is_not(raises(json.decoder.JSONDecodeError)),
+                    "Response returned non-json body")
+
+    @staticmethod
+    def assert_status_code_is_equal(actual_status_code, expected_status_code):
+        assert_that(actual_status_code, equal_to(expected_status_code), 'Status code is incorrect')
+
+    @staticmethod
+    def assert_response_body_is_equal(actual_body, expected_body):
+        assert_that(actual_body, equal_to(expected_body), 'Response body is incorrect')
+
     @pytest.mark.test_id(1)
     def test_create_bear(self, api_object):
         logging.log(logging.INFO, f'1. Отправить POST-запрос на эндпоинт {api_object.BEAR_ENDPOINT_URL}')
@@ -20,30 +33,29 @@ class TestAlaskaBears:
                           bear_id=None)
         response = api_object.create_bear(bear_model)
         bear_id = int(response.text) if response.text.isnumeric() else None
-        assert_that(response.status_code, equal_to(HTTPStatus.OK), 'Status code is incorrect')
+        self.assert_status_code_is_equal(response.status_code, HTTPStatus.OK)
         assert_that(bear_id, not_none(), 'Response should contain bear_id')
 
         logging.log(logging.INFO, f'2. Отправить GET-запрос на эндпоинт {api_object.BEAR_ENDPOINT_URL}')
         bear_model.bear_id = bear_id
         response = api_object.get_bears_list()
         actual = [Bear(**entry) for entry in response.json()]
-        assert_that(response.status_code, equal_to(HTTPStatus.OK), 'Status code is incorrect')
+        self.assert_status_code_is_equal(response.status_code, HTTPStatus.OK)
         assert_that(bear_model, is_in(actual), f'Response should contain the Bear but wasn\'t: {bear_model}')
 
     @pytest.mark.test_id(2)
     def test_get_bear_list(self, api_object):
         logging.log(logging.INFO, f'1. Отправить GET-запрос на эндпоинт {api_object.BEAR_ENDPOINT_URL}')
         response = api_object.get_bears_list()
-        assert_that(response.status_code, equal_to(HTTPStatus.OK), 'Status code is incorrect')
-        assert_that(calling(response.json), is_not(raises(json.decoder.JSONDecodeError)),
-                    "Response returned non-json body")
-        expected_content_ = response.text
+        self.assert_status_code_is_equal(response.status_code, HTTPStatus.OK)
+        self.assert_response_is_in_json_format(response)
+        expected_content_ = [Bear(**entry) for entry in response.json()]
         logging.log(logging.INFO, f'2. Отправить GET-запрос на эндпоинт {api_object.BEAR_ENDPOINT_URL}')
         response = api_object.get_bears_list()
-        assert_that(response.status_code, equal_to(HTTPStatus.OK), 'Status code is incorrect')
-        assert_that(calling(response.json), is_not(raises(json.decoder.JSONDecodeError)),
-                    "Response returned non-json body")
-        assert_that(response.text, equal_to(expected_content_), "Response returned incorrect body")
+        self.assert_status_code_is_equal(response.status_code, HTTPStatus.OK)
+        self.assert_response_is_in_json_format(response)
+        actual_content = [Bear(**entry) for entry in response.json()]
+        self.assert_response_body_is_equal(actual_content, expected_content_)
 
     @pytest.mark.test_id(3)
     def test_get_bear(self, api_object, create_bear_with_valid_data):
@@ -51,19 +63,18 @@ class TestAlaskaBears:
 
         logging.log(logging.INFO, f'1. Отправить GET-запрос на эндпоинт {api_object.BEAR_ID_ENDPOINT_URL.format(id=bear_id)}')
         response = api_object.get_bear(bear_id)
-        assert_that(response.status_code, equal_to(HTTPStatus.OK), 'Status code is incorrect')
-        assert_that(calling(response.json), is_not(raises(json.decoder.JSONDecodeError)),
-                    "Response returned non-json body")
+        self.assert_status_code_is_equal(response.status_code, HTTPStatus.OK)
+        self.assert_response_is_in_json_format(response)
         actual = Bear(**response.json())
         assert_that(actual.bear_id, equal_to(bear_id), 'Response returned bear with incorrect bear_id')
-        expected_body = response.text
+        expected_content_ = actual
 
         logging.log(logging.INFO, f'2. Отправить GET-запрос на эндпоинт {api_object.BEAR_ID_ENDPOINT_URL.format(id=bear_id)}')
         response = api_object.get_bear(bear_id)
-        assert_that(response.status_code, equal_to(HTTPStatus.OK), 'Status code is incorrect')
-        assert_that(calling(response.json), is_not(raises(json.decoder.JSONDecodeError)),
-                    "Response returned non-json body")
-        assert_that(response.text, equal_to(expected_body), 'Response returned incorrect body')
+        self.assert_status_code_is_equal(response.status_code, HTTPStatus.OK)
+        self.assert_response_is_in_json_format(response)
+        actual_content = Bear(**response.json())
+        self.assert_response_body_is_equal(actual_content, expected_content_)
 
     @pytest.mark.test_id(4)
     def test_update_bear(self, api_object, create_bear_with_valid_data):
@@ -75,46 +86,25 @@ class TestAlaskaBears:
                           bear_age=constants.UpdateBearData.BEAR_AGE,
                           bear_id=bear_id)
         response = api_object.update_bear(bear_model)
-        assert_that(response.status_code, equal_to(HTTPStatus.OK), 'Status code is incorrect')
-        assert_that(response.text, equal_to(constants.ResponseMessages.SUCCESS), 'Response body is incorrect')
+        self.assert_status_code_is_equal(response.status_code, HTTPStatus.OK)
+        self.assert_response_body_is_equal(response.text, constants.ResponseMessages.SUCCESS)
 
         logging.log(logging.INFO, f'2. Отправить GET-запрос на эндпоинт {api_object.BEAR_ID_ENDPOINT_URL.format(id=bear_id)}')
         response = api_object.get_bear(bear_model.bear_id)
         actual = Bear(**response.json())
-        assert_that(response.status_code, equal_to(HTTPStatus.OK), 'Status code is incorrect')
-        assert_that(actual, dataclass_equals(bear_model), f'Response should contain the Bear but wasn\'t: {bear_model}')
-
-        logging.log(logging.INFO, f'3. Отправить PUT-запрос на эндпоинт {api_object.BEAR_ID_ENDPOINT_URL.format(id=bear_id)} с payload шага 1')
-        response = api_object.update_bear(bear_model)
-        assert_that(response.status_code, equal_to(HTTPStatus.CREATED), 'Status code is incorrect')
-        assert_that(response.text, equal_to(constants.ResponseMessages.SUCCESS), 'Response body is incorrect')
-
-        logging.log(logging.INFO, f'4. Отправить GET-запрос на эндпоинт {api_object.BEAR_ID_ENDPOINT_URL.format(id=bear_id)}')
-        response = api_object.get_bear(bear_model.bear_id)
-        actual = Bear(**response.json())
-        assert_that(response.status_code, equal_to(HTTPStatus.OK), 'Status code is incorrect')
+        self.assert_status_code_is_equal(response.status_code, HTTPStatus.OK)
         assert_that(actual, dataclass_equals(bear_model), f'Response should contain the Bear but wasn\'t: {bear_model}')
 
     @pytest.mark.test_id(5)
     def test_delete_all_bears(self, api_object):
         logging.log(logging.INFO, f'1. Отправить DELETE-запрос на эндпоинт {api_object.BEAR_ENDPOINT_URL}')
         response = api_object.delete_all_bears()
-        assert_that(response.status_code, equal_to(HTTPStatus.OK), 'Status code is incorrect')
-        assert_that(response.text, equal_to(constants.ResponseMessages.SUCCESS), 'Response body is incorrect')
+        self.assert_status_code_is_equal(response.status_code, HTTPStatus.OK)
+        self.assert_response_body_is_equal(response.text, constants.ResponseMessages.SUCCESS)
 
         logging.log(logging.INFO, f'2. Отправить GET-запрос на эндпоинт {api_object.BEAR_ENDPOINT_URL}')
         response = api_object.get_bears_list()
-        assert_that(response.status_code, equal_to(HTTPStatus.OK), 'Status code is incorrect')
-        assert_that(response.json(), empty(), 'Response body is incorrect')
-
-        logging.log(logging.INFO, f'3. Отправить DELETE-запрос на эндпоинт {api_object.BEAR_ENDPOINT_URL}')
-        response = api_object.delete_all_bears()
-        assert_that(response.status_code, equal_to(HTTPStatus.OK), 'Status code is incorrect')
-        assert_that(response.text, equal_to(constants.ResponseMessages.SUCCESS), 'Response body is incorrect')
-
-        logging.log(logging.INFO, f'4. Отправить GET-запрос на эндпоинт {api_object.BEAR_ENDPOINT_URL}')
-        response = api_object.get_bears_list()
-        assert_that(response.status_code, equal_to(HTTPStatus.OK), 'Status code is incorrect')
+        self.assert_status_code_is_equal(response.status_code, HTTPStatus.OK)
         assert_that(response.json(), empty(), 'Response body is incorrect')
 
     @pytest.mark.test_id(6)
@@ -123,33 +113,22 @@ class TestAlaskaBears:
 
         logging.log(logging.INFO, f'1. Отправить DELETE-запрос на эндпоинт {api_object.BEAR_ID_ENDPOINT_URL.format(id=bear_id)}')
         response = api_object.delete_bear(bear_id)
-        assert_that(response.status_code, equal_to(HTTPStatus.OK), 'Status code is incorrect')
-        assert_that(response.text, equal_to(constants.ResponseMessages.SUCCESS), 'Response body is incorrect')
+        self.assert_status_code_is_equal(response.status_code, HTTPStatus.OK)
+        self.assert_response_body_is_equal(response.text, constants.ResponseMessages.SUCCESS)
 
         logging.log(logging.INFO, f'2. Отправить GET-запрос на эндпоинт {api_object.BEAR_ENDPOINT_URL}')
         response = api_object.get_bears_list()
         actual = [Bear(**entry) for entry in response.json()]
-        assert_that(response.status_code, equal_to(HTTPStatus.OK), 'Status code is incorrect')
+        self.assert_status_code_is_equal(response.status_code, HTTPStatus.OK)
         assert_that([bear for bear in actual if bear.bear_id == bear_id], empty(),
                     f'Response contains bear with id "{bear_id}" but should\'nt')
-        saved_response_text = response.text
-
-        logging.log(logging.INFO, f'3. Отправить DELETE-запрос на эндпоинт {api_object.BEAR_ID_ENDPOINT_URL.format(id=bear_id)}')
-        response = api_object.delete_bear(bear_id)
-        assert_that(response.status_code, equal_to(HTTPStatus.OK), 'Status code is incorrect')
-        assert_that(response.text, equal_to(constants.ResponseMessages.SUCCESS), 'Response body is incorrect')
-
-        logging.log(logging.INFO, f'4. Отправить GET-запрос на эндпоинт {api_object.BEAR_ENDPOINT_URL}')
-        response = api_object.get_bears_list()
-        assert_that(response.status_code, equal_to(HTTPStatus.OK), 'Status code is incorrect')
-        assert_that(response.text, equal_to(saved_response_text), 'Response is incorrect')
 
     @pytest.mark.test_id(7)
     def test_get_not_existing_bear(self, api_object, not_existing_bear_id):
         logging.log(logging.INFO, f'1. Отправить GET-запрос на эндпоинт {api_object.BEAR_ID_ENDPOINT_URL.format(id=not_existing_bear_id)}')
         response = api_object.get_bear(not_existing_bear_id)
-        assert_that(response.status_code, equal_to(HTTPStatus.OK), 'Status code is incorrect')
-        assert_that(response.text, equal_to(constants.ResponseMessages.EMPTY), 'Response body is incorrect')
+        self.assert_status_code_is_equal(response.status_code, HTTPStatus.OK)
+        self.assert_response_body_is_equal(response.text, constants.ResponseMessages.EMPTY)
 
     @pytest.mark.test_id(9)
     def test_update_not_existing_bear(self, api_object, not_existing_bear_id):
@@ -159,11 +138,11 @@ class TestAlaskaBears:
                           bear_name=constants.UpdateBearData.BEAR_NAME,
                           bear_age=constants.UpdateBearData.BEAR_AGE)
         response = api_object.update_bear(bear_model)
-        assert_that(response.status_code, equal_to(HTTPStatus.NOT_FOUND), 'Status code is incorrect')
+        self.assert_status_code_is_equal(response.status_code, HTTPStatus.NOT_FOUND)
 
     @pytest.mark.test_id(10)
     def test_delete_not_existing_bear(self, api_object, not_existing_bear_id):
         logging.log(logging.INFO, f'1. Отправить DELETE-запрос на эндпоинт {api_object.BEAR_ID_ENDPOINT_URL.format(id=not_existing_bear_id)}')
         response = api_object.delete_bear(not_existing_bear_id)
-        assert_that(response.status_code, equal_to(HTTPStatus.OK), 'Status code is incorrect')
-        assert_that(response.text, equal_to(constants.ResponseMessages.SUCCESS), 'Response body is incorrect')
+        self.assert_status_code_is_equal(response.status_code, HTTPStatus.OK)
+        self.assert_response_body_is_equal(response.text, constants.ResponseMessages.SUCCESS)
