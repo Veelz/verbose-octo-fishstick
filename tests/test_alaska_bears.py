36f6331e867879e49ce986c4cd64a21db5f0a71d
v.osipov <v.osipov@a1qa.com>
2022-04-11 16:17:14 +0300

    Add allure steps

--- tests/test_alaska_bears.py
+++ tests/test_alaska_bears.py
@@ -1,7 +1,7 @@
 import json
-import logging
 from http import HTTPStatus
 
+import allure
 import pytest
 from hamcrest import assert_that, equal_to, not_none, is_in, empty, calling, is_not, raises
 
@@ -24,113 +24,112 @@ class TestAlaskaBears:
     def assert_response_body_is_equal(actual_body, expected_body):
         assert_that(actual_body, equal_to(expected_body), 'Response body is incorrect')
 
+    @allure.title("1. Создать запись о медведе")
     @pytest.mark.test_id(1)
     @pytest.mark.parametrize('bear_type', (constants.BearType.BLACK, constants.BearType.BROWN,
                                            constants.BearType.GUMMY, constants.BearType.POLAR))
     def test_create_bear(self, api_object, bear_type):
-        logging.log(logging.INFO, f'1. Отправить POST-запрос на эндпоинт {api_object.BEAR_ENDPOINT_URL}')
-        bear_model = Bear(bear_type=bear_type,
-                          bear_name=constants.CreateValidBearData.BEAR_NAME,
-                          bear_age=constants.CreateValidBearData.BEAR_AGE,
-                          bear_id=None)
-        response = api_object.create_bear(bear_model)
-        bear_id = int(response.text) if response.text.isnumeric() else None
-        self.assert_status_code_is_equal(response.status_code, HTTPStatus.OK)
-        assert_that(bear_id, not_none(), 'Response should contain bear_id')
+        with allure.step(f'1. Отправить POST-запрос на эндпоинт {api_object.BEAR_ENDPOINT_URL}'):
+            bear_model = Bear(bear_type=bear_type,
+                              bear_name=constants.CreateValidBearData.BEAR_NAME,
+                              bear_age=constants.CreateValidBearData.BEAR_AGE,
+                              bear_id=None)
+            response = api_object.create_bear(bear_model)
+            bear_id = int(response.text) if response.text.isnumeric() else None
+            self.assert_status_code_is_equal(response.status_code, HTTPStatus.OK)
+            assert_that(bear_id, not_none(), 'Response should contain bear_id')
 
-        logging.log(logging.INFO, f'2. Отправить GET-запрос на эндпоинт {api_object.BEAR_ENDPOINT_URL}')
-        bear_model.bear_id = bear_id
-        response = api_object.get_bears_list()
-        actual = [Bear(**entry) for entry in response.json()]
-        self.assert_status_code_is_equal(response.status_code, HTTPStatus.OK)
-        assert_that(bear_model, is_in(actual), f'Response should contain the Bear but wasn\'t: {bear_model}')
+        with allure.step(f'2. Отправить GET-запрос на эндпоинт {api_object.BEAR_ENDPOINT_URL}'):
+            bear_model.bear_id = bear_id
+            response = api_object.get_bears_list()
+            actual = [Bear(**entry) for entry in response.json()]
+            self.assert_status_code_is_equal(response.status_code, HTTPStatus.OK)
+            assert_that(bear_model, is_in(actual), f'Response should contain the Bear but wasn\'t: {bear_model}')
 
-        logging.log(logging.INFO, f'3. Отправить GET-запрос на эндпоинт {api_object.BEAR_ID_ENDPOINT_URL.format(id=bear_id)}')
-        response = api_object.get_bear(bear_id)
-        actual = Bear(**response.json())
-        self.assert_status_code_is_equal(response.status_code, HTTPStatus.OK)
-        assert_that(actual, dataclass_equals(bear_model), f'Response should contain the Bear but wasn\'t: {bear_model}')
-
+    @allure.title("3. Получить запись о медведе")
     @pytest.mark.test_id(3)
     def test_get_bear(self, api_object, create_bear_with_valid_data):
         bear_id = create_bear_with_valid_data.bear_id
+        with allure.step(f'1. Отправить GET-запрос на эндпоинт {api_object.BEAR_ID_ENDPOINT_URL.format(id=bear_id)}'):
+            response = api_object.get_bear(bear_id)
+            self.assert_status_code_is_equal(response.status_code, HTTPStatus.OK)
+            self.assert_response_is_in_json_format(response)
+            actual = Bear(**response.json())
+            assert_that(actual.bear_id, equal_to(bear_id), 'Response returned bear with incorrect bear_id')
 
-        logging.log(logging.INFO, f'1. Отправить GET-запрос на эндпоинт {api_object.BEAR_ID_ENDPOINT_URL.format(id=bear_id)}')
-        response = api_object.get_bear(bear_id)
-        self.assert_status_code_is_equal(response.status_code, HTTPStatus.OK)
-        self.assert_response_is_in_json_format(response)
-        actual = Bear(**response.json())
-        assert_that(actual.bear_id, equal_to(bear_id), 'Response returned bear with incorrect bear_id')
-
+    @allure.title("4. Обновить запись о медведе")
     @pytest.mark.test_id(4)
     @pytest.mark.parametrize('bear_type', (constants.BearType.BLACK, constants.BearType.BROWN,
                                            constants.BearType.GUMMY, constants.BearType.POLAR))
     def test_update_bear(self, api_object, create_bear_with_valid_data, bear_type):
         bear_id = create_bear_with_valid_data.bear_id
+        with allure.step(f'1. Отправить PUT-запрос на эндпоинт {api_object.BEAR_ID_ENDPOINT_URL.format(id=bear_id)}'):
+            bear_model = Bear(bear_type=bear_type,
+                              bear_name=constants.UpdateBearData.BEAR_NAME,
+                              bear_age=constants.UpdateBearData.BEAR_AGE,
+                              bear_id=bear_id)
+            response = api_object.update_bear(bear_model)
+            self.assert_status_code_is_equal(response.status_code, HTTPStatus.OK)
+            self.assert_response_body_is_equal(response.text, constants.ResponseMessages.SUCCESS)
 
-        logging.log(logging.INFO, f'1. Отправить PUT-запрос на эндпоинт {api_object.BEAR_ID_ENDPOINT_URL.format(id=bear_id)}')
-        bear_model = Bear(bear_type=bear_type,
-                          bear_name=constants.UpdateBearData.BEAR_NAME,
-                          bear_age=constants.UpdateBearData.BEAR_AGE,
-                          bear_id=bear_id)
-        response = api_object.update_bear(bear_model)
-        self.assert_status_code_is_equal(response.status_code, HTTPStatus.OK)
-        self.assert_response_body_is_equal(response.text, constants.ResponseMessages.SUCCESS)
+        with allure.step(f'2. Отправить GET-запрос на эндпоинт {api_object.BEAR_ID_ENDPOINT_URL.format(id=bear_id)}'):
+            response = api_object.get_bear(bear_model.bear_id)
+            actual = Bear(**response.json())
+            self.assert_status_code_is_equal(response.status_code, HTTPStatus.OK)
+            assert_that(actual, dataclass_equals(bear_model), f'Response should contain the Bear but wasn\'t: {bear_model}')
 
-        logging.log(logging.INFO, f'2. Отправить GET-запрос на эндпоинт {api_object.BEAR_ID_ENDPOINT_URL.format(id=bear_id)}')
-        response = api_object.get_bear(bear_model.bear_id)
-        actual = Bear(**response.json())
-        self.assert_status_code_is_equal(response.status_code, HTTPStatus.OK)
-        assert_that(actual, dataclass_equals(bear_model), f'Response should contain the Bear but wasn\'t: {bear_model}')
-
+    @allure.title("5. Удалить записи о всех медведях")
     @pytest.mark.test_id(5)
     def test_delete_all_bears(self, api_object):
-        logging.log(logging.INFO, f'1. Отправить DELETE-запрос на эндпоинт {api_object.BEAR_ENDPOINT_URL}')
-        response = api_object.delete_all_bears()
-        self.assert_status_code_is_equal(response.status_code, HTTPStatus.OK)
-        self.assert_response_body_is_equal(response.text, constants.ResponseMessages.SUCCESS)
+        with allure.step(f'1. Отправить DELETE-запрос на эндпоинт {api_object.BEAR_ENDPOINT_URL}'):
+            response = api_object.delete_all_bears()
+            self.assert_status_code_is_equal(response.status_code, HTTPStatus.OK)
+            self.assert_response_body_is_equal(response.text, constants.ResponseMessages.SUCCESS)
 
-        logging.log(logging.INFO, f'2. Отправить GET-запрос на эндпоинт {api_object.BEAR_ENDPOINT_URL}')
-        response = api_object.get_bears_list()
-        self.assert_status_code_is_equal(response.status_code, HTTPStatus.OK)
-        assert_that(response.json(), empty(), 'Response body is incorrect')
+        with allure.step(f'2. Отправить GET-запрос на эндпоинт {api_object.BEAR_ENDPOINT_URL}'):
+            response = api_object.get_bears_list()
+            self.assert_status_code_is_equal(response.status_code, HTTPStatus.OK)
+            assert_that(response.json(), empty(), 'Response body is incorrect')
 
+    @allure.title("6. Удалить запись о медведе")
     @pytest.mark.test_id(6)
     def test_delete_bear(self, api_object, create_bear_with_valid_data):
         bear_id = create_bear_with_valid_data.bear_id
+        with allure.step(f'1. Отправить DELETE-запрос на эндпоинт {api_object.BEAR_ID_ENDPOINT_URL.format(id=bear_id)}'):
+            response = api_object.delete_bear(bear_id)
+            self.assert_status_code_is_equal(response.status_code, HTTPStatus.OK)
+            self.assert_response_body_is_equal(response.text, constants.ResponseMessages.SUCCESS)
 
-        logging.log(logging.INFO, f'1. Отправить DELETE-запрос на эндпоинт {api_object.BEAR_ID_ENDPOINT_URL.format(id=bear_id)}')
-        response = api_object.delete_bear(bear_id)
-        self.assert_status_code_is_equal(response.status_code, HTTPStatus.OK)
-        self.assert_response_body_is_equal(response.text, constants.ResponseMessages.SUCCESS)
+        with allure.step(f'2. Отправить GET-запрос на эндпоинт {api_object.BEAR_ENDPOINT_URL}'):
+            response = api_object.get_bears_list()
+            actual = [Bear(**entry) for entry in response.json()]
+            self.assert_status_code_is_equal(response.status_code, HTTPStatus.OK)
+            assert_that([bear for bear in actual if bear.bear_id == bear_id], empty(),
+                        f'Response contains bear with id "{bear_id}" but should\'nt')
 
-        logging.log(logging.INFO, f'2. Отправить GET-запрос на эндпоинт {api_object.BEAR_ENDPOINT_URL}')
-        response = api_object.get_bears_list()
-        actual = [Bear(**entry) for entry in response.json()]
-        self.assert_status_code_is_equal(response.status_code, HTTPStatus.OK)
-        assert_that([bear for bear in actual if bear.bear_id == bear_id], empty(),
-                    f'Response contains bear with id "{bear_id}" but should\'nt')
-
+    @allure.title("7. Получить несуществующую запись о медведе")
     @pytest.mark.test_id(7)
     def test_get_not_existing_bear(self, api_object, not_existing_bear_id):
-        logging.log(logging.INFO, f'1. Отправить GET-запрос на эндпоинт {api_object.BEAR_ID_ENDPOINT_URL.format(id=not_existing_bear_id)}')
-        response = api_object.get_bear(not_existing_bear_id)
-        self.assert_status_code_is_equal(response.status_code, HTTPStatus.OK)
-        self.assert_response_body_is_equal(response.text, constants.ResponseMessages.EMPTY)
+        with allure.step(f'1. Отправить GET-запрос на эндпоинт {api_object.BEAR_ID_ENDPOINT_URL.format(id=not_existing_bear_id)}'):
+            response = api_object.get_bear(not_existing_bear_id)
+            self.assert_status_code_is_equal(response.status_code, HTTPStatus.OK)
+            self.assert_response_body_is_equal(response.text, constants.ResponseMessages.EMPTY)
 
+    @allure.title("9. Обновить запись несуществующего медведя")
     @pytest.mark.test_id(9)
     def test_update_not_existing_bear(self, api_object, not_existing_bear_id):
-        logging.log(logging.INFO, f'1. Отправить PUT-запрос на эндпоинт {api_object.BEAR_ID_ENDPOINT_URL.format(id=not_existing_bear_id)}')
-        bear_model = Bear(bear_id=not_existing_bear_id,
-                          bear_type=constants.UpdateBearData.BEAR_TYPE,
-                          bear_name=constants.UpdateBearData.BEAR_NAME,
-                          bear_age=constants.UpdateBearData.BEAR_AGE)
-        response = api_object.update_bear(bear_model)
-        self.assert_status_code_is_equal(response.status_code, HTTPStatus.NOT_FOUND)
+        with allure.step(f'1. Отправить PUT-запрос на эндпоинт {api_object.BEAR_ID_ENDPOINT_URL.format(id=not_existing_bear_id)}'):
+            bear_model = Bear(bear_id=not_existing_bear_id,
+                              bear_type=constants.UpdateBearData.BEAR_TYPE,
+                              bear_name=constants.UpdateBearData.BEAR_NAME,
+                              bear_age=constants.UpdateBearData.BEAR_AGE)
+            response = api_object.update_bear(bear_model)
+            self.assert_status_code_is_equal(response.status_code, HTTPStatus.NOT_FOUND)
 
+    @allure.title("10. Удалить запись несуществующего медведя")
     @pytest.mark.test_id(10)
     def test_delete_not_existing_bear(self, api_object, not_existing_bear_id):
-        logging.log(logging.INFO, f'1. Отправить DELETE-запрос на эндпоинт {api_object.BEAR_ID_ENDPOINT_URL.format(id=not_existing_bear_id)}')
-        response = api_object.delete_bear(not_existing_bear_id)
-        self.assert_status_code_is_equal(response.status_code, HTTPStatus.OK)
-        self.assert_response_body_is_equal(response.text, constants.ResponseMessages.SUCCESS)
+        with allure.step(f'1. Отправить DELETE-запрос на эндпоинт {api_object.BEAR_ID_ENDPOINT_URL.format(id=not_existing_bear_id)}'):
+            response = api_object.delete_bear(not_existing_bear_id)
+            self.assert_status_code_is_equal(response.status_code, HTTPStatus.OK)
+            self.assert_response_body_is_equal(response.text, constants.ResponseMessages.SUCCESS)
